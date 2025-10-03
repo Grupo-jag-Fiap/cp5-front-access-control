f42d32a107181acec95e5f5e0c59be35969434d4
yJoaoVictor10 <jvna07@gmail.com>
2025-10-02 23:26:16 -0300

    chore: adicionando estilos globais com tailwind e configura frmEditarUsuario

--- access-control-cp/src/main.tsx
+++ access-control-cp/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Error from './Error/index.tsx'
 import Login from './Login/index.tsx'
+import './globals.css'
 
 const router = createBrowserRouter([{
   path:"/", element:<App/>, errorElement:<Error/>, children:[
