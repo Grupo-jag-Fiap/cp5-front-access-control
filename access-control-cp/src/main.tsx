5be00df23edf64e151ccd125ddcfc2e5fd2e55a5
yJoaoVictor10 <jvna07@gmail.com>
2025-10-02 23:17:16 -0300

    feat: configura rotas e renderiza aplicação

--- access-control-cp/src/main.tsx
+++ access-control-cp/src/main.tsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Error from './Error/index.tsx'
+import Login from './Login/index.tsx'
+
+const router = createBrowserRouter([{
+  path:"/", element:<App/>, errorElement:<Error/>, children:[
+    {path:"/", element:<Login/>},
+  ]
+}])
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
