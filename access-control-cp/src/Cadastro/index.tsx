76005191d9b07b368f12faed733775a1f94783f0
Asuyz <aemygdioferreira@gmail.com>
2025-10-04 20:20:42 -0300

    chore:  Inialização do useForm e estrutura inicial de handle Submit

--- access-control-cp/src/Cadastro/index.tsx
+++ access-control-cp/src/Cadastro/index.tsx
@@ -5,6 +5,15 @@ import { useNavigate } from "react-router-dom";
 const API_URL = import.meta.env.VITE_API_URL_USUARIOS;
 
 export default function Cadastro(){
+
+
+    const navigateLogin = useNavigate();
+
+  const { register, handleSubmit, formState: { errors } } = useForm<TipoUsuario>({
+    mode: "onChange"
+  });
+
+
   return (
     <main>
       <h1>Cadastrar usuários</h1>
