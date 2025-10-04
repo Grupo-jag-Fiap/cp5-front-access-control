dc00948bf08d16d2699d2b069f123e38ad9cdcc8
Asuyz <aemygdioferreira@gmail.com>
2025-10-04 20:32:09 -0300

    feat: Implementação de submissão no formulário | cadastra e trata erros

--- access-control-cp/src/Cadastro/index.tsx
+++ access-control-cp/src/Cadastro/index.tsx
@@ -6,13 +6,41 @@ const API_URL = import.meta.env.VITE_API_URL_USUARIOS;
 
 export default function Cadastro(){
 
-
-    const navigateLogin = useNavigate();
+  const navigateLogin = useNavigate();
 
   const { register, handleSubmit, formState: { errors } } = useForm<TipoUsuario>({
     mode: "onChange"
   });
 
+  const onSubmit = handleSubmit(async (data: TipoUsuario) => {
+    try {
+      const res = await fetch(API_URL);
+      const usuarios: TipoUsuario[] = await res.json();
+
+      const usuarioJaExiste = usuarios.filter(
+        (u) =>
+          u.nomeUsuario.toLowerCase() === data.nomeUsuario.toLowerCase() &&
+          u.email.toLowerCase() === data.email.toLowerCase()
+      );
+
+      if (usuarioJaExiste) {
+        alert("Usuário já cadastrado");
+        return;
+      }
+
+      await fetch(API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      alert("Usuário cadastrado com sucesso!");
+      navigateLogin("/");
+    } catch (error) {
+      console.error(error);
+      alert("Erro ao cadastrar.");
+    }
+  });
 
   return (
     <main>
