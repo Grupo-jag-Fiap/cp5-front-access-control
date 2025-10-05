ea4fe478d2b465d7bb1b85251a901a5808f0721a
Asuyz <aemygdioferreira@gmail.com>
2025-10-04 22:28:26 -0300

    feat: Criação do feedback para o usuário e estilização básica

--- access-control-cp/src/Cadastro/index.tsx
+++ access-control-cp/src/Cadastro/index.tsx
@@ -50,19 +50,22 @@ export default function Cadastro(){
   const usuario = JSON.parse(localStorage.getItem("usuarioLogado") || "null");
 
 
- return (
-    <main>
+     return(
+ <main>
       <h1>Cadastrar usuários</h1>
       <div className="text-center bg-amber-100">
-        {usuario ? (
-          <div className="usuario-logado">
-            <p className="bemvindo">Bem-vindo, {usuario.nome}</p>
-            <button onClick={handleLogout} className="logout-btn">Sair</button>
-          </div>
+      {usuario ? (
+        <div className="usuario-logado">
+          <p className="bemvindo">Bem-vindo, {usuario.nome}</p>
+          <button onClick={handleLogout} className="logout-btn">Sair</button>
+        </div>
         ) : (
           <p className="text-gray-700 font-medium mb-4">Bem-vindo visitante</p>
         )}
+
       </div>
+
+
 
       <div className="mx-auto w-[40%]">
         <form onSubmit={onSubmit} className="frmEditarUsuario">
@@ -70,35 +73,30 @@ export default function Cadastro(){
             <legend>Usuário</legend>
             <div>
               <label htmlFor="idNome">Nome:</label>
-              <input
-              type="text"
-               id="idNome"
-              {...register("nome", { required: true, maxLength: 200, minLength: 3 })}
-              />
+              <input type="text" id="idNome" className="bg-amber-50" {...register("nome", { required: true ,maxLength: 200, minLength:3})} 
+              aria-invalid={!!errors.nome} aria-describedby={errors.nome ? "nome-error" : undefined} />
+              {errors.nome && <span role="alert" id="nome-error" className="text-red-600">Digite um nome válido!</span>} 
             </div>
             <div>
               <label htmlFor="idNomeUsuario">Nome de usuário:</label>
-              <input
-              type="text"
-              id="idNomeUsuario"
-              {...register("nomeUsuario", { required: true, maxLength: 200, minLength: 3 })}
-              />
+              <input type="text" id="idNomeUsuario" className="bg-amber-50" {...register("nomeUsuario", { required: true ,maxLength: 200, minLength:3 })} 
+              aria-invalid={!!errors.nomeUsuario} aria-describedby={errors.nomeUsuario ? "nomeUsuario-error" : undefined} />
+              {errors.nomeUsuario && <span role="alert" id="nomeUsuario-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">Nome de usuário inválido!</span>}
             </div>
             <div>
               <label htmlFor="idEmail">E-mail do usuário:</label>
-              <input
-              type="email"
-              id="idEmail"
-              {...register("email", { required: true, minLength: 5, maxLength: 255 })}
-              />
+              <input type="email" id="idEmail" className="bg-amber-50" {...register("email",{required: true, minLength:5,maxLength:255})} 
+              aria-invalid={!!errors.email} aria-describedby={errors.email ? "email-error" : undefined} />
+              {errors.email && <span role="alert" id="email-error" className="text-red-600">Email inválido.</span>}
             </div>
             <div>
               <button type="submit">Cadastrar</button>
-              <button onClick={() => navigateLogin("/")}>Login</button>
+              <button onClick={()=> navigateLogin("/")}>Login</button>
             </div>
           </fieldset>
         </form>
+
       </div>
     </main>
-  );;
+    )
 }
